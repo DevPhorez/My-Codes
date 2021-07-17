9951b184fd4581309e545cb0fc687c4542291e09
phorez <phorez@yahoo.com>
2021-07-17 19:26:06 +0430

    Update Filter By and Fixed Price Filter

--- filters.py
+++ filters.py
@@ -1,9 +1,9 @@
 # views codes
-request = self.request
 color = request.GET.get('color')
 sort = request.GET.get('sorted')
 size = request.GET.get('size')
-price = request.GET.get('price')
+price_min = request.GET.get('min')
+price_max = request.GET.get('max')
 
 order = None
 if sort == 'cheapest':
@@ -13,48 +13,50 @@ elif sort == 'latest':
 elif sort == 'popularity':
     order = '-view_count'
 
-if sort is not None and color is not None and size is not None and price is not None:
-    return Products.objects.get_color_size_price_sorting(color, size, price, order)
-elif sort is not None and color is not None and size is not None:
+if (sort and color and size and price_min and price_max) is not None:
+    return Products.objects.get_color_size_price_sorting(color, size, price_min, price_max, order)
+elif (sort and color and size) is not None:
     return Products.objects.get_color_size_sorting(color, size, order)
-elif sort is not None and color is not None:
+elif (sort and size) is not None:
     return Products.objects.get_color_sorting(color, order)
-elif sort is not None and size is not None:
+elif (sort and size) is not None:
     return Products.objects.get_size_sorting(size, order)
-elif color is not None and size is not None:
+elif (color and size) is not None:
     return Products.objects.get_color_size(color, size)
-elif sort is not None and price is not None:
-    return Products.objects.get_price_sorting(price, order) 
-elif color is not None and price is not None:
-    return Products.objects.get_color_price(color, price) 
-elif size is not None and price is not None:
-    return Products.objects.get_size_price(size, price)                                    
+elif (sort and price_min and price_max) is not None:
+    return Products.objects.get_price_sorting(price_min, price_max, order)
+elif (color and price_min and price_max) is not None:
+    return Products.objects.get_color_price(color, price_min, price_max)
+elif (size and price_min and price_max) is not None:
+    return Products.objects.get_size_price(size, price_min, price_max)
 elif color is not None:
     return Products.objects.get_color(color)
 elif sort is not None:
     return Products.objects.is_active().order_by(f'{order}')
 elif size is not None:
     return Products.objects.get_size(size)
-elif price is not None:
-    return Products.objects.get_price(price)            
+elif (price_min and price_max) is not None:
+    return Products.objects.get_price(price_min, price_max)
+elif (price_min or price_max) is not None:
+    raise Http404('')
 else:
     return Products.objects.is_active()
 
 # models codes
-def get_color_size_price_sorting(self, COLOR, SIZE, PRICE, SORT_BY):
-    return self.get_queryset().filter(color=COLOR, size=SIZE, price=PRICE).order_by(f'{SORT_BY}')
+def get_color_size_price_sorting(self, COLOR, SIZE, PRICE_MIN, PRICE_MAX, SORT_BY):
+    return self.get_queryset().filter(color=COLOR, size=SIZE, price__range=(PRICE_MIN, PRICE_MAX)).order_by(f'{SORT_BY}')
 
-def get_price_sorting(self, PRICE, SORT_BY):
-    return self.get_queryset().filter(price=PRICE).order_by(f'{SORT_BY}')      
+def get_price_sorting(self, PRICE_MIN, PRICE_MAX, SORT_BY):
+    return self.get_queryset().filter(price__range=(PRICE_MIN, PRICE_MAX)).order_by(f'{SORT_BY}')
 
-def get_color_price(self, COLOR, PRICE):
-    return self.get_queryset().filter(color=COLOR, price=PRICE)
+def get_color_price(self, COLOR, PRICE_MIN, PRICE_MAX):
+    return self.get_queryset().filter(color=COLOR, price__range=(PRICE_MIN, PRICE_MAX))
 
-def get_size_price(self, SIZE, PRICE):
-    return self.get_queryset().filter(size=SIZE, price=PRICE)        
+def get_size_price(self, SIZE, PRICE_MIN, PRICE_MAX):
+    return self.get_queryset().filter(size=SIZE, price__range=(PRICE_MIN, PRICE_MAX))
 
-def get_price(self, PRICE):
-    return self.get_queryset().filter(price=PRICE)
+def get_price(self, PRICE_MIN, PRICE_MAX):
+    return self.get_queryset().filter(price__range=(PRICE_MIN, PRICE_MAX))
 
 def get_color_size_sorting(self, COLOR, SIZE, SORT_BY):
     if COLOR == '2' and SIZE == '6':
@@ -85,3 +87,5 @@ def get_color(self, COLOR):
     if COLOR == '2':
         return self.get_queryset().filter(active=True)
     return self.get_queryset().filter(color=COLOR)
+if COLOR == '2':
+    return self.get_queryset().filter(active=True)
